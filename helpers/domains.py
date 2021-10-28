73ff471c102dcdef58ba54ca1440887f40f13d12
Jackson Nestler <24984165+jdoescyber@users.noreply.github.com>
2021-10-28 08:28:27 -0700

    Now parsing out the JSON data for domains.

--- helpers/domains.py
+++ helpers/domains.py
@@ -23,16 +23,38 @@ def checkThreatCrowd(domainFileLocation):
         with open(domainFileLocation, "r") as file:
             for line in file:
             
-                # This is the API endpoint for IP lookups.
+                # This is the API endpoint for domain lookups.
                 result =  requests.get("https://www.threatcrowd.org/searchApi/v2/domain/report/", params = {"domain": line})
-                jsonData = json.loads(result.text)
-                if jsonData['response_code'] == "1":
-                    text.printGreen("Found data for " + line)
-                    print (jsonData)
-                    # TODO: find some sample response and parse it out.
-                elif jsonData['response_code'] == "0":
-                    text.printRed("No results for domain: " + line)
-                    #print ("**DEBUG**: " + result.text) # Uncomment line to show the JSON response.
+                if result.status_code == 200:
+                    jsonData = json.loads(result.text)
+                    if jsonData['response_code'] == "1": # ThreatCrowd uses its own 0 and 1 response codes, different from HTTP status codes.
+                        text.printGreen("Found data for " + line)
+                        
+                        # We'll examine information about DNS resolution history.
+                        for resolutions in jsonData['resolutions']: # for each result we get back...
+                            resolvedTime = resolutions['last_resolved'] # Grab the time it was resolved at.
+                            resolvedIP = resolutions['ip_address'] # Grab the IP the domain resolved to.
+                            print("Domain had IP " + str(resolvedIP) + " on date " + resolvedTime)
+
+                        # Now we check for subdomains
+                        for subdomain in jsonData['subdomains']:
+                            print("Known subdomain: " + str(subdomain))
+
+                        # Check for any external references to support findings.
+                        for reference in jsonData['references']:
+                            print("Reference: " + str(reference))
+                        
+                        # Finally, look at the overall votes.
+                        communityScore = jsonData['votes']
+                        communityScore = str(communityScore)
+                        print("The domain has an overall vote of " + communityScore + ". This is a community score.")
+
+                    elif jsonData['response_code'] == "0": # If ThreatCrowd doesn't find any results.
+                        text.printRed("No results for domain: " + line)
+                        #print ("**DEBUG**: " + result.text) # Uncomment line to show the JSON response.
+                else: 
+                    # If the web server turns a non-200 HTTP status code.
+                    print("ThreatCrowd API returned a non-200 status code. API may be down or you may be rate limited.")
                 
                 # ThreatCrowd requests a sleep of at least 10 seconds between requests, but allows bursting.
                 # Subsequently, we're going to sleep between 5 and 10 seconds to be kind.
@@ -46,4 +68,4 @@ def checkThreatCrowd(domainFileLocation):
                 #print ("sleeping for " + str(courtesySleepDuration)) # if you'd like to see how long you're sleeping for, uncomment.
                 time.sleep(courtesySleepDuration)
     except Exception as e:
-        print(e + "...the ThreatCrowd API may be down.")
+        print(e)
